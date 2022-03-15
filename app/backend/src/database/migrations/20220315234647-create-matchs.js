d5e7f5118e41855b4219919c196728eb51e6611b
Matheus <ms.conrado25@hotmail.com>
2022-03-15 20:51:26 -0300

    implementando a migration matchs

--- app/backend/src/database/migrations/20220315234647-create-matchs.js
+++ app/backend/src/database/migrations/20220315234647-create-matchs.js
@@ -2,20 +2,37 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Matchs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      home_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      home_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      away_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      away_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      in_progress: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Matchs');
   }
 };
