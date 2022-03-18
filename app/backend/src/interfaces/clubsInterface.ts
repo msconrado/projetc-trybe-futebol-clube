85d9ea5e3cd2736e4c5a84ce009ce832d6425efe
Matheus <ms.conrado25@hotmail.com>
2022-03-18 12:03:05 -0300

    criando a interface IUpdateGols

--- app/backend/src/interfaces/clubsInterface.ts
+++ app/backend/src/interfaces/clubsInterface.ts
@@ -2,6 +2,10 @@ export interface IId {
   id: string;
 }
 
+export interface IUpdateGols extends IId {
+  homeTeamGoals: number;
+  awayTeamGoals: number;
+}
 export interface IHomeAway {
   homeTeam: string;
   awayTeam: string;
