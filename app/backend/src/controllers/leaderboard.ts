21b5ee46d5d4f65b39b9f8306fd3f96796f581ef
Matheus <ms.conrado25@hotmail.com>
2022-03-21 10:23:38 -0300

    criando o controller do leaderboard/home

--- app/backend/src/controllers/leaderboard.ts
+++ app/backend/src/controllers/leaderboard.ts
@@ -4,6 +4,7 @@ import leaderboardService from '../services/leaderboard';
 const getAll = async (_req: Request, res: Response, next: NextFunction) => {
   try {
     const table = await leaderboardService.getAll();
+    leaderboardService.getAllHome();
 
     return res.status(200).json(table);
   } catch (error) {
@@ -11,4 +12,15 @@ const getAll = async (_req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default { getAll };
+const getAllHome = async (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    const tableHome = await leaderboardService.getAllHome();
+    await leaderboardService.getAll();
+
+    return res.status(200).json(tableHome);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { getAll, getAllHome };
